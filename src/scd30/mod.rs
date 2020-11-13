d6b79f4f4e6b14323398a8facfc1930b667ea8ce
Mirabellensaft <tanks@ferrous-systems.com>
2020-11-13 15:21:05 +0100

    write measuring related methods

--- src/scd30/mod.rs
+++ src/scd30/mod.rs
@@ -30,16 +30,22 @@ where
         Ok([major, minor])
     }
 
-    pub fn start_measuring(&mut self) -> Result<(), Error> {
-        let pressure = 0_u16;
-
+    pub fn start_continuous_measurement(&mut self, pressure: u16) -> Result<(), Error> {
+        
         // command bytes
         let mut command: [u8; 5] = [0x00, 0x10, 0x00, 0x00, 0x00];
+        let argument_bytes = &pressure.to_be_bytes();
+
+        command[2] = argument_bytes[0];
+        command[3] = argument_bytes[1];
 
         let mut crc = Crc::<u8>::new(0x31, 8, 0xff, 0x00, false);
 
+        defmt::info!("{:?}", command);
+
         crc.update(&pressure.to_be_bytes());
         command[4] = crc.finish();
+        defmt::info!("{:?}", command);
 
         self.0.write(DEFAULT_ADDRESS, &command)?;
 
@@ -56,7 +62,7 @@ where
         Ok(u16::from_be_bytes([rd_buffer[0], rd_buffer[1]]) == 1)
     }
 
-    pub fn get_measurement(&mut self) -> Result<SensorData, Error> {
+    pub fn read_measurement(&mut self) -> Result<SensorData, Error> {
         let command: [u8; 2] = [0x03, 0x00];
         let mut rd_buffer = [0u8; 18];
 
@@ -87,3 +93,41 @@ where
     }
 }
 
+// pub fn get_self_calibration_status<T: Instance>() -> Result<bool, Error> {
+//         let mut command:[u8; 2] = [0x53, 0x06];
+//         let mut rd_buffer = [0u8; 3];
+    
+//         self.0.write(DEFAULT_ADDRESS, &command).unwrap();
+//         self.0.read(DEFAULT_ADDRESS, &mut rd_buffer).unwrap();
+    
+    
+    // pub fn get_frc_value<T: Instance>(DEFAULT_ADDRESS: u8, i2c: &mut Twim<T>) -> Result<u16, Error> {
+    //     let mut command:[u8; 2] = [0x52, 0x04];
+    //     let mut rd_buffer = [0u8; 2];
+    
+    //     self.0.write(DEFAULT_ADDRESS, &command).unwrap();
+    //     self.0.read(DEFAULT_ADDRESS, &mut rd_buffer).unwrap();
+    
+    //     Ok(u16::from_be_bytes(rd_buffer))
+    // }
+    
+    // pub fn get_measurement_interval<T: Instance>(DEFAULT_ADDRESS: u8, i2c: &mut Twim<T>) -> Result<u16, Error> {
+    //     let mut command:[u8; 2] = [0x46, 0x00];
+    //     let mut rd_buffer = [0u8; 2];
+    
+    //     self.0.write(DEFAULT_ADDRESS, &command).unwrap();
+    //     self.0.read(DEFAULT_ADDRESS, &mut rd_buffer).unwrap();
+    
+    //     Ok(u16::from_be_bytes(rd_buffer))
+    // }
+    
+    // pub fn get_firmware<T: Instance>(DEFAULT_ADDRESS: u8, i2c: &mut Twim<T>) -> Result<u16, Error> {
+    //     let mut command:[u8; 2] = [0xd1, 0x00];
+    //     let mut rd_buffer = [0u8; 2];
+    
+    //     i2c.write(DEFAULT_ADDRESS, &command).unwrap();
+    //     i2c.read(DEFAULT_ADDRESS, &mut rd_buffer).unwrap();
+    
+    //     Ok(u16::from_be_bytes(rd_buffer))
+    // }
+    
